812a6f1ac18e06bd2d310265b51e9e54cb0d1f39
rmoscuba <rmoscuba@gmail.com>
2021-08-24 10:09:04 -0400

    VIstas CRUD Estudiante

--- studentscrud/urls.py
+++ studentscrud/urls.py
@@ -6,4 +6,7 @@ app_name = 'studentscrud'
 urlpatterns = [
     path('', views.StudentsView.as_view(), name='estudiantes'),
     path('<int:pk>/', views.StudentView.as_view(), name='estudiante'),
+    path('nuevo/', views.EstudianteCreateView.as_view(), name='estudiante-nuevo'),
+    path('<int:pk>/editar', views.EstudianteUpdateView.as_view(), name='estudiante-editar'),
+    path('<int:pk>/borrar/', views.EstudianteDeleteView.as_view(), name='estudiante-borrar'),
 ]
--- studentscrud/views.py
+++ studentscrud/views.py
@@ -5,6 +5,11 @@ from django.views import generic
 
 from .models import Estudiante
 
+from django.views.generic.edit import FormView
+
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.urls import reverse_lazy
+
 class StudentsView(generic.ListView):
     template_name = 'studentscrud/estudiantes.html'
     context_object_name = 'lista_de_estudiantes'
@@ -16,3 +21,31 @@ class StudentsView(generic.ListView):
 class StudentView(generic.DetailView):
     model = Estudiante
     template_name = 'studentscrud/estudiante.html'
+
+class EstudianteCreateView(CreateView):
+    model = Estudiante
+    fields = [
+        'nombre',
+        'sexo',
+        'email',
+        'fecha_nacimiento',
+        'ciudad_nacimiento',
+        'grupo'
+        ]
+    success_url = reverse_lazy('studentscrud:estudiantes')
+
+class EstudianteUpdateView(UpdateView):
+    model = Estudiante
+    fields = [
+        'nombre',
+        'sexo',
+        'email',
+        'fecha_nacimiento',
+        'ciudad_nacimiento',
+        'grupo'
+        ]
+    success_url = reverse_lazy('studentscrud:estudiantes')
+
+class EstudianteDeleteView(DeleteView):
+    model = Estudiante
+    success_url = reverse_lazy('studentscrud:estudiantes')
