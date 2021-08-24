060d7252f327e15c99e9e720205d6938a7ee26c5
rmoscuba <rmoscuba@gmail.com>
2021-08-24 10:27:54 -0400

    Organizo las URLs

--- students/urls.py
+++ students/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('estudiantes/', include('studentscrud.urls')),
+    path('', include('studentscrud.urls')),
     path('admin/', admin.site.urls),
 ]
