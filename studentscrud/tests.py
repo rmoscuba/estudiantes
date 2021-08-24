46424e057fea34cc79c6039ee5816e202fa595bc
rmoscuba <rmoscuba@gmail.com>
2021-08-23 22:30:25 -0400

    Tests para Estudiantes

--- studentscrud/tests.py
+++ studentscrud/tests.py
@@ -1,3 +1,65 @@
 from django.test import TestCase
 
-# Create your tests here.
+import datetime
+
+from django.test import TestCase
+
+from .models import Estudiante, Profesor, Ciudad, Grupo
+
+from django.core.exceptions import ValidationError 
+
+class EstudianteModelTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.profe = Profesor(nombre="Pedro")
+        cls.profe.save()
+        cls.grupo = Grupo(nombre="A1", profesor_guia=cls.profe)
+        cls.grupo.save()
+        cls.ciudad = Ciudad(nombre="Holguín")
+        cls.ciudad.save()
+
+    def test_prueba_que_no_se_guarde_estudiante_con_email_none(self):
+
+        nuevo_estudiante = Estudiante(
+            nombre="Alfredito", 
+            sexo="M", 
+            email=None, 
+            ciudad_nacimiento = self.ciudad,
+            fecha_nacimiento = datetime.datetime(1977, 12, 6),
+            grupo = self.grupo)
+        try:
+            nuevo_estudiante.full_clean()
+        except Exception as e:
+            self.assertEqual(e.__str__(), "{'email': ['This field cannot be null.']}")
+
+    def test_prueba_que_no_se_guarde_estudiante_con_email_vacio(self):
+
+        nuevo_estudiante = Estudiante(
+            nombre="Alfredito", 
+            sexo="M", 
+            email='', 
+            ciudad_nacimiento = self.ciudad,
+            fecha_nacimiento = datetime.datetime(1977, 12, 6),
+            grupo = self.grupo)
+        try:
+            nuevo_estudiante.full_clean()
+        except Exception as e:
+            self.assertEqual(e.__str__(), "{'email': ['This field cannot be blank.']}")
+
+    def test_prueba_que_no_se_guarde_estudiante_con_email_incorrecto(self):
+
+        nuevo_estudiante = Estudiante(
+            nombre="Alfredito", 
+            sexo="M", 
+            email='emailmalformado', 
+            ciudad_nacimiento = self.ciudad,
+            fecha_nacimiento = datetime.datetime(1977, 12, 6),
+            grupo = self.grupo)
+        try:
+            nuevo_estudiante.full_clean()
+            self.fail()
+        except Exception as e:
+            self.assertEqual(e.__str__(), "{'email': ['Enter a valid email address']}")
+
+    
